f9e6b8e3c92225acebcb198a95dec311a4f53948
Acharya Group <acharyasumit786@gmail.com>
2025-12-24 16:12:28 +0530

    update domain

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const PORT = process.env.PORT || 4000;
 const allowedOrigins = [
   "http://localhost:3000",
   "https://msplyoga.com",
+  "http://msplyoga.com",
   "https://mspl-beta.vercel.app"
 ];
 
